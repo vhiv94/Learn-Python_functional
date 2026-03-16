9b9bcbeffdb6586ff3d2a6328945fd6b84a47341
vhiv94 <vhiv94@gmail.com>
2026-03-16 14:34:57 -0500

    Pass Lesson 4:zipmap

--- Ch4/L4.py
+++ Ch4/L4.py
@@ -1,6 +1,10 @@
-def main_func(keys: list[str], values: list[float]) -> dict[str, float]:
+def zipmap(keys: list[str], values: list[float]) -> dict[str, float]:
+    if len(keys) == 0 or len(values) == 0:
+        return {}
 
-    return dict(zip(keys, values))
+    result = zipmap(keys[1:], values[1:])
+    result[keys[0]] = values[0]
+    return result
 
 run_cases = [
     (
@@ -51,3 +55,49 @@ submit_cases = run_cases + [
         },
     ),
 ]
+
+def print_dict(d):
+    for key, value in sorted(d.items()):
+        print(f" * {key}: {value}")
+
+
+def test(keys, values, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {keys}, {values}")
+    print("Expected:")
+    print_dict(expected_output)
+    result = zipmap(keys, values)
+    print("Actual:")
+    print_dict(result)
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()

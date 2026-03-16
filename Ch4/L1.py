db0ebf4ee6731b04d88ea881340aab2bc0e853b7
vhiv94 <vhiv94@gmail.com>
2026-03-16 13:12:26 -0500

    Seperate L1

--- Ch4/L1.py
+++ Ch4/L1.py
@@ -14,28 +14,3 @@ submit_cases = run_cases + [
     (2, 2),
     (10, 3628800),
 ]
-
-## Another Example
-def print_chars(word, i):
-    if i == len(word):
-        return
-    print(word[i])
-    print_chars(word, i + 1)
-
-print_chars("elephant", 3)
-# H
-# e
-# l
-# l
-# o
-
-## Lesson 2: What happens if a recursive function has no base case?
-# It's no longer considered recursive
-# *It will keep calling itself forever, resulting in an infinite loop
-# Alan Turing comes back from the dead to haunt the author
-
-## Lesson 3: What is printed if I call print_chars('elephant', 3)?
-# phant
-# elephant
-# ele
-# ant

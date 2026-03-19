7bdf68b95af4391132628520edf5b974044ef3d4
vhiv94 <vhiv94@gmail.com>
2026-03-19 12:07:24 -0500

    Add Lesson 7: lru_cache and explore example with debugger

---
+++ Ch8/L7_ex.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+
+@lru_cache()
+def factorial_r(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial_r(x - 1)
+
+factorial_r(10) # no previously cached result, makes 11 recursive calls
+# 3628800
+factorial_r(5)  # just looks up cached value result
+# 120
+factorial_r(12) # makes two new recursive calls, the other 11 are cached
+# 479001600
